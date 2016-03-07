4de36343f645e4b039c6dfcd5f72c1425cbeb61f
Ryan Skinner <anomaly1452@gmail.com>
2016-03-07 11:18:31 -0700

    Run higher-resolution results for Problem 3.

--- matlab/Generate_G.m
+++ matlab/Generate_G.m
@@ -11,11 +11,16 @@ x = linspace(-a,a,Nx);
 [lh, phih] = Analytical_Eigs(sigma, ell, a, b, x);
 
 X = ones(realizations,Nx); % Initialize to one because <G(x)> = 1.0;
+hwait = waitbar(0,'Generating realizations of G...');
 for r = 1:realizations
+    if mod(r,10) == 0
+        waitbar(r / realizations, hwait)
+    end
     Y = randn(1,b);
     for i = 1:b
         X(r,:) = X(r,:) + sqrt(lh(i)) * phih(:,i)' * Y(i);
     end
 end
+close(hwait)
 
 end
--- matlab/Problem_3.m
+++ matlab/Problem_3.m
@@ -1,4 +1,4 @@
-function [] = Problem_2()
+function [] = Problem_3()
     
     Set_Default_Plot_Properties();
     
@@ -20,14 +20,9 @@ function [] = Problem_2()
     uf  = randn(1, realizations) * sqrt(0.1); % Random variable theta ~ N(0, 0.1)
     
     % Generate K
+    fprintf('Generating %i (~%.0e) realizations...\n',realizations,realizations);
     G = Generate_G(N, realizations);
     K = exp(G);
-
-    % Solve for u
-    u = nan(realizations, N);
-    for numb = 1:realizations
-        u(numb,:) = Solve_u(h, u0, uf(numb), K(numb,:));
-    end
     
     % Plot realizations of G.
     figure();
@@ -35,11 +30,24 @@ function [] = Problem_2()
     plot(x, G(1:10,:));
     ylabel('G(x,\omega)');
     xlabel('x');
+    
     % Plot realizations of K.
     subplot(3,1,2);
     plot(x, K(1:10,:));
     ylabel('K(x,\omega)');
     xlabel('x');
+
+    % Solve for u
+    u = nan(realizations, N);
+    hwait = waitbar(0,'Solving heat equations...');
+    for numb = 1:realizations
+        if mod(numb,10) == 0
+            waitbar(numb / realizations, hwait)
+        end
+        u(numb,:) = Solve_u(h, u0, uf(numb), K(numb,:));
+    end
+    close(hwait)
+    
     % Plot realizations of u.
     subplot(3,1,3);
     plot(x, u(1:10,:));
@@ -48,10 +56,17 @@ function [] = Problem_2()
     
     % Plot statistics of u.
     figure();
-    n = [100, 300, 1000, 3000, 10000, 30000, 100000];
-%     n = [10, 30, 100, 300, 1000, 3000, 10000];
-%     n = [10, 30, 100, 300, 1000];
-%     n = [10, 30];
+    if realizations >= 100000
+        n = [100, 300, 1000, 3000, 10000, 30000, 100000];
+    elseif realizations >= 10000
+        n = [10, 30, 100, 300, 1000, 3000, 10000];
+    elseif realizations >= 1000
+        n = [10, 30, 100, 300, 1000];
+    elseif realizations >= 100
+        n = [10, 30, 100];
+    else
+        n = [1];
+    end
     rgb = flip(copper(length(n)));
     index = 0;
     for numb = n
@@ -82,23 +97,6 @@ function [] = Problem_2()
     set(hleg2, 'Location', 'eastoutside');
     
     % Calculate probability of excessive max temperature.
-    n = 100:10000;
-    prob = nan(1,length(n));
-    for numb = 1:length(n)
-        umax = max(u(1:n(numb),:),[],2);
-        m = mean(umax);
-        v = var(umax);
-        hits = sum( umax > ( m + 3*sqrt(v) ) );
-        prob(numb) = hits / n(numb);
-    end
-    figure();
-    hold on;
-    plot(n, prob);
-    plot([n(1),n(end)], prob(end)*[1,1], '--');
-    set(gca, 'XScale', 'log');
-    xlabel('Cumulative Realizations');
-    ylabel('Probability');
-    
     umax = max(u,[],2);
     m = mean(umax);
     v = var(umax);
@@ -107,6 +105,31 @@ function [] = Problem_2()
     fprintf('  E(umax) = %.4e\n', m);
     fprintf('Var(umax) = %.4e\n', v);
     fprintf('Probability of excessive temperature: %.4e\n', prob);
+    
+    % Plot convergence is probability.
+    if n > 100
+        n = 100:10000;
+        hwait = waitbar(0,'Calculating probability plot data...');
+        prob = nan(1,length(n));
+        for numb = 1:length(n)
+            if mod(numb,10) == 0
+                waitbar(numb / length(n), hwait)
+            end
+            umax = max(u(1:n(numb),:),[],2);
+            m = mean(umax);
+            v = var(umax);
+            hits = sum( umax > ( m + 3*sqrt(v) ) );
+            prob(numb) = hits / n(numb);
+        end
+        close(hwait)
+        figure();
+        hold on;
+        plot(n, prob);
+        plot([n(1),n(end)], prob(end)*[1,1], '--');
+        set(gca, 'XScale', 'log');
+        xlabel('Cumulative Realizations');
+        ylabel('Probability');
+    end
     
 end
 
